b46c37957e5708f92ef6cbf34df7ada37f0d56e3
alinakassym <alinakassym@gmail.com>
2025-07-28 23:32:57 +0500

    feat: add checkbox toggle handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import styles from "./App.module.css";
-import { useRequestGetList, useRequestCreateTodo } from "./hooks";
+import {
+  useRequestGetList,
+  useRequestCreateTodo,
+  useRequestUpdateTodo,
+} from "./hooks";
 import { TodoForm, TodoList } from "./components";
 
 function App() {
@@ -8,16 +12,22 @@ function App() {
   const refresh = () => setRefreshFlag(!refreshFlag);
   const { list, isLoading } = useRequestGetList(refreshFlag);
   const { createTodo } = useRequestCreateTodo(refresh);
+  const { updateTodo } = useRequestUpdateTodo(refresh);
 
   const handleAdd = (newTodo) => {
     createTodo(newTodo);
+  };
+
+  const handleToggle = (todo) => {
+    const { id, completed } = todo;
+    updateTodo(id, !completed);
   };
 
   return (
     <div className={styles.App}>
       <h1 className={styles.header}>Todo list</h1>
       <TodoForm onAdd={handleAdd} />
-      <TodoList list={list} isLoading={isLoading} />
+      <TodoList list={list} isLoading={isLoading} onToggle={handleToggle} />
     </div>
   );
 }
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,6 +1,6 @@
 import styles from "./TodoList.module.css";
 
-const TodoList = ({ list, isLoading }) => {
+const TodoList = ({ list, isLoading, onToggle }) => {
   return (
     <div className={styles.list}>
       {isLoading ? (
@@ -9,7 +9,11 @@ const TodoList = ({ list, isLoading }) => {
         list.map((listItem) => (
           <div key={listItem.id} className={styles.listItem}>
             <div className={styles.listItemTitle}>
-              <input type="checkbox" checked={listItem.completed} />
+              <input
+                type="checkbox"
+                checked={listItem.completed}
+                onChange={() => onToggle(listItem)}
+              />
               <div className={styles.listItemCheckbox}>{listItem.title}</div>
             </div>
             <button className={styles.deleteButton}>Delete</button>
