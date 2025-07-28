baf5103bf535d8459370cddd2812d1fa32f62ee8
alinakassym <alinakassym@gmail.com>
2025-07-28 22:54:24 +0500

    feat: create TodoForm

---
+++ src/components/TodoForm/TodoForm.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import styles from "./TodoForm.module.css";
+
+const TodoForm = ({ onAdd }) => {
+  const [title, setTitle] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!title.trim()) return;
+    onAdd(title);
+    setTitle("");
+  };
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <input
+        className={styles.input}
+        type="text"
+        value={title}
+        onChange={({ target }) => setTitle(target.value)}
+        placeholder="Add new todo..."
+      />
+      <button className={styles.button} type="submit">
+        Add
+      </button>
+    </form>
+  );
+};
+
+export default TodoForm;
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,2 @@
 export { default as TodoList } from "./TodoList/TodoList";
+export { default as TodoForm } from "./TodoForm/TodoForm";
