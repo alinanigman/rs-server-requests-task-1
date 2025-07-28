28b6c23bcddb75317a680b9235ba8ce142dd7ded
alinakassym <alinakassym@gmail.com>
2025-07-28 20:13:29 +0500

    feat: create hook useRequestGetList

---
+++ src/hooks/useRequestGetList.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+export const useRequestGetList = () => {
+  const [list, setList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((data) => {
+        setList(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching list:", error);
+        setIsLoading(false);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+  return {
+    list,
+    isLoading,
+  };
+};
