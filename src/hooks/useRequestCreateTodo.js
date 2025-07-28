f2107b28cfb9089c98c62263d17e5ca3620b8b82
alinakassym <alinakassym@gmail.com>
2025-07-28 22:55:49 +0500

    refactor: useRequestCreateTodo

--- src/hooks/useRequestCreateTodo.js
+++ src/hooks/useRequestCreateTodo.js
@@ -1,10 +1,22 @@
-export const useRequestCreateTodo = () => {
+export const useRequestCreateTodo = (refresh) => {
   const createTodo = (todoText) => {
     return fetch("http://localhost:3000/todos", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ title: todoText, completed: false }),
-    }).then((res) => res.json());
+    })
+      .then((res) => res.json())
+      .then((createdTodo) => {
+        console.log("Created todo:", createdTodo);
+        return createdTodo;
+      })
+      .catch((error) => {
+        console.error("Error creating todo:", error);
+        throw error;
+      })
+      .finally(() => {
+        refresh();
+      });
   };
 
   return { createTodo };
