7bbf6d88081e65e598850939bc685e87926f147d
alinakassym <alinakassym@gmail.com>
2025-07-28 20:08:27 +0500

    styles: update root, add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import styles from "./App.module.css";
 function App() {
-  return <></>;
+  return (
+    <div className={styles.App}>
+      <div className={styles.header}>Todo list</div>
+    </div>
+  );
 }
 
 export default App;
