c004f859b050028c6df2301922fde4eea6c36ec1
alinakassym <alinakassym@gmail.com>
2025-07-29 17:52:54 +0500

    feat: create HomePage & TaskPage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,44 @@
+import styles from "../App.module.css";
+import {
+  useRequestGetList,
+  useRequestCreateTodo,
+  useRequestUpdateTodo,
+  useRequestDeleteTodo,
+} from "../hooks";
+import { TodoForm, TodoList } from "../components";
+
+function HomePage() {
+  const { list, isLoading } = useRequestGetList();
+  const { isCreating, createTodo } = useRequestCreateTodo();
+  const { isUpdating, updateTodo } = useRequestUpdateTodo();
+  const { isDeleting, deleteTodo } = useRequestDeleteTodo();
+
+  const handleAdd = (newTodo) => {
+    createTodo(newTodo);
+  };
+
+  const handleToggle = (todo) => {
+    updateTodo(todo);
+  };
+
+  const handleDelete = (todoId) => {
+    deleteTodo(todoId);
+  };
+
+  return (
+    <div className={styles.App}>
+      <h1 className={styles.header}>Todo list</h1>
+      <TodoForm isCreating={isCreating} onAdd={handleAdd} />
+      <TodoList
+        list={list}
+        isLoading={isLoading}
+        isUpdating={isUpdating}
+        isDeleting={isDeleting}
+        onToggle={handleToggle}
+        onDelete={handleDelete}
+      />
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ src/pages/TaskPage.jsx
@@ -0,0 +1,7 @@
+import styles from "../App.module.css";
+
+function TaskPage() {
+  return <div className={styles.App}>taskPage</div>;
+}
+
+export default TaskPage;
