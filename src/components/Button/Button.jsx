9987e1de4a1e82f92ee5de69a0576dbf2a4324d3
alinakassym <alinakassym@gmail.com>
2025-08-12 01:02:35 +0500

    feat: add props for color, size, and disabled state to Button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,10 @@
 import styles from "./Button.module.css";
 
-const Button = ({ color = "primary", onClick, children, small }) => {
+const Button = ({ color = "primary", onClick, children, small, disabled }) => {
   let btnClassName = `${styles.Button} ${styles[color]}`;
   if (small) btnClassName += ` ${styles.small}`;
   return (
-    <button className={btnClassName} onClick={onClick}>
+    <button className={btnClassName} onClick={onClick} disabled={disabled}>
       {children}
     </button>
   );
